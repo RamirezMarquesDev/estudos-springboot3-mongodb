44ebfd6ce27598567182460f3710848adf2e3585
RamirezMarquesDev <ramirezmarquescampello@gmail.com>
2024-03-20 11:22:34 -0300

    Criado projeto para PetShop com mongoDB

---
+++ src/test/java/com/example/petshopmongo/PetshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
