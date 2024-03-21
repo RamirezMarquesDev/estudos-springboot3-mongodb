f45af9cd621a2255b97746dbb0ee69b26828fae5
RamirezMarquesDev <ramirezmarquescampello@gmail.com>
2024-03-21 11:34:43 -0300

    Mudando para padrão DTO

--- src/main/java/com/ramirez/petshopmongo/resourses/UserResource.java
+++ src/main/java/com/ramirez/petshopmongo/resourses/UserResource.java
@@ -1,6 +1,7 @@
 package com.ramirez.petshopmongo.resourses;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ramirez.petshopmongo.domain.User;
+import com.ramirez.petshopmongo.dto.UserDTO;
 import com.ramirez.petshopmongo.services.UserService;
 
 @RestController
@@ -20,8 +22,9 @@ public class UserResource {
 
 	// @RequestMapping(method = RequestMethod.GET)
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {	
+	public ResponseEntity<List<UserDTO>> findAll() {	
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
