14a4a127913ef7a2cca627a4d80f4ead0f630832
RamirezMarquesDev <ramirezmarquescampello@gmail.com>
2024-03-21 12:34:45 -0300

    Metodo de select via id e tratamento de erro caso id nao existente

--- src/main/java/com/ramirez/petshopmongo/services/UserService.java
+++ src/main/java/com/ramirez/petshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.ramirez.petshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ramirez.petshopmongo.domain.User;
 import com.ramirez.petshopmongo.repository.UserRepository;
+import com.ramirez.petshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,10 @@ public class UserService {
 		
 		return repo.findAll();
 	}
-}
+	
+	public User findById(String id) {
+			Optional<User> obj = repo.findById(id);
+			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+			}
+	}
+
