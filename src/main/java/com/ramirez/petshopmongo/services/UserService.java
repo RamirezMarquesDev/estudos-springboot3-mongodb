1315898f37b8aac41042b17d28db7a31516a2735
RamirezMarquesDev <ramirezmarquescampello@gmail.com>
2024-03-22 09:35:03 -0300

    Adicionado usuario com POST

--- src/main/java/com/ramirez/petshopmongo/services/UserService.java
+++ src/main/java/com/ramirez/petshopmongo/services/UserService.java
@@ -7,23 +7,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ramirez.petshopmongo.domain.User;
+import com.ramirez.petshopmongo.dto.UserDTO;
 import com.ramirez.petshopmongo.repository.UserRepository;
 import com.ramirez.petshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
-		
+
+	public List<User> findAll() {
+
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
-			Optional<User> obj = repo.findById(id);
-			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-			}
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
+	}
+}
