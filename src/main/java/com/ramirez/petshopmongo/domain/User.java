4b2ba918272b1da5dd793b0652a9ee1790d925bb
RamirezMarquesDev <ramirezmarquescampello@gmail.com>
2024-03-23 10:09:37 -0300

    Criado referencia dos Posts

--- src/main/java/com/ramirez/petshopmongo/domain/User.java
+++ src/main/java/com/ramirez/petshopmongo/domain/User.java
@@ -1,23 +1,29 @@
 package com.ramirez.petshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "user")//<--- opcional o spring mapeia automaticamente buscando nome da classe
-public class User implements Serializable{
+@Document(collection = "user") // <--- opcional o spring mapeia automaticamente buscando nome da classe
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private String name;
 	private String email;
-	
+
+	@DBRef(lazy = true) // <--- lazy = true -> não carrega os posts quando carrega o usuario
+	private List<Post> posts = new ArrayList<>();
+
 	public User() {
-		
+
 	}
-	
+
 	public User(String id, String name, String email) {
 		super();
 		this.id = id;
@@ -44,9 +50,17 @@ public class User implements Serializable{
 	public String getId() {
 		return id;
 	}
-	
+
 	public void setId(String id) {
 		this.id = id;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
