4b098078b23af38a1cc4bc77fa85f1d7f6231886
RamirezMarquesDev <ramirezmarquescampello@gmail.com>
2024-03-23 12:00:42 -0300

    Criado comentários dos posts

--- src/main/java/com/ramirez/petshopmongo/config/Instantiation.java
+++ src/main/java/com/ramirez/petshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.ramirez.petshopmongo.domain.Post;
 import com.ramirez.petshopmongo.domain.User;
 import com.ramirez.petshopmongo.dto.AuthorDTO;
+import com.ramirez.petshopmongo.dto.ComentDTO;
 import com.ramirez.petshopmongo.repository.PostRepository;
 import com.ramirez.petshopmongo.repository.UserRepository;
 
@@ -43,6 +44,14 @@ public class Instantiation implements CommandLineRunner {
 				"vou viajar para Noruega. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("22/03/2024 10:54:00"), "Bom Dia!",
 				"Acordei e ja fui logo estudando hoje", new AuthorDTO(maria));
+
+		ComentDTO c1 = new ComentDTO("Boa viagem Mary!", sdf.parse("23/03/2024 11:51:10"), new AuthorDTO(alex));
+		ComentDTO c2 = new ComentDTO("Aproveite!", sdf.parse("23/03/2024 11:52:10"), new AuthorDTO(bob));
+		ComentDTO c3 = new ComentDTO("Se for beber não dirija", sdf.parse("23/03/2024 12:54:33"), new AuthorDTO(bob));
+		ComentDTO c4 = new ComentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2024 11:53:10"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c3));
+		post2.getComments().addAll(Arrays.asList(c2, c4));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
